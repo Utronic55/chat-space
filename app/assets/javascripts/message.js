1603e0c9267a7f9b463ba32c7fd190165461fcf9
Utronic55 <halfboiled5@gmail.com>
2020-01-14 11:47:22 +0900

    jquery.fix.1

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -42,8 +42,6 @@ $(function(){
     e.preventDefault();
     var formData = new FormData(this);
     var url = $(this).attr('action')
-    console.log(formData)
-    console.log(url)
     $.ajax({
       url: url,
       type: "POST",
